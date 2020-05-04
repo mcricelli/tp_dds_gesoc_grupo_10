21516ee50a0ef69088c70b8064abb8fcedd637cd
Marco Castillo <marco.andres.cricelli@gmail.com>
2020-05-04 00:22:55 -0300

    Se implementan metodos EsValida y ObtenerMotivosInvalidez

--- src/GeSoc/Clave/ValidadorClave.java
+++ src/GeSoc/Clave/ValidadorClave.java
@@ -1,5 +1,21 @@
 package GeSoc.Clave;
 
-public class ValidadorClave {
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class ValidadorClave
+{
+	private List<CriterioValidacion> criteriosValidacion;
+	
+	public Boolean EsValida(String clave)
+	{
+		return criteriosValidacion.stream().allMatch( criterio -> criterio.EsValida(clave));
+	}
+	
+	public List<String> ObtenerMotivosInvalidez(String clave)
+	{
+		return criteriosValidacion.stream().filter(criterio -> criterio.EsValida(clave))
+				.map(criterio -> criterio.ObtenerMotivoInvalidez(clave))
+				.collect(Collectors.toList());
+	}
 }
