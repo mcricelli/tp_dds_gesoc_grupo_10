9521bf416bbbe0bf9e72895629e07f7e1d85448a
Jose_Martinez <martinez022jose@gmail.com>
2020-05-05 14:43:42 -0300

    Se agrega 'test por longitud'

--- src/GeSoc/Clave/Validaciones/ValidacionPorLongitud.java
+++ src/GeSoc/Clave/Validaciones/ValidacionPorLongitud.java
@@ -3,10 +3,15 @@ package GeSoc.Clave.Validaciones;
 import GeSoc.Clave.CriterioValidacion;
 
 public class ValidacionPorLongitud implements CriterioValidacion {
-	private int minimo = 8;
-	private int maximo = 64;
+	public int minimo = 8;
+	public int maximo = 64;
 	private String errorMinimo = "El minimo de caracteres permitidos es: ";
 	private String errorMaximo = " El maximo de caracteres permitidos es: ";
+	
+	public ValidacionPorLongitud(int rangoMinimo,int rangoMaximo) {
+		this.minimo = rangoMinimo;
+		this.maximo = rangoMaximo;
+	}
 
 	@Override
 	public Boolean EsValida(String clave) {
---
+++ test/GeSoc/Clave/TestValidacionPorLongitud.java
@@ -0,0 +1,64 @@
+package GeSoc.Clave;
+import GeSoc.Clave.Validaciones.ValidacionPorLongitud;
+
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Before;
+
+
+public class TestValidacionPorLongitud{
+	
+	private String contrasenia;
+	private ValidadorClave validador;
+	
+	
+	
+	@Before
+	public void init() {
+		validador = new ValidadorClave();
+		validador.AgregarCriterio(new ValidacionPorLongitud(8,64));
+		
+	}
+	
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	
+	public void generarClave(String clave) {
+		this.contrasenia = clave;
+	}
+	
+
+	@Test
+	public void noDeAceptarClaveMenorAOcho() {
+		this.generarClave("hola");
+		Assert.assertFalse(validador.EsValida(contrasenia));
+        
+	}
+	
+	@Test
+	public void noDebeContarEspaciosEnBlanco() {
+		this.generarClave("hol      a");
+		Assert.assertFalse(validador.EsValida(contrasenia));
+	}
+	
+	@Test
+	public void noDebeSuperarElMaximo() {
+		this.generarClave("holacomoestasholacomoestasholacomoestasholacomoestasholacomoestas");
+		Assert.assertFalse(validador.EsValida(contrasenia));
+	}
+	
+	@Test
+	public void noDebeAceptarEspaciosVaciosInicioOFin() {
+		this.generarClave("  hola    ");
+		Assert.assertFalse(validador.EsValida(contrasenia));
+	}
+	
+	@Test
+	public void debeAceptarDentroDelRango() {
+		this.generarClave("holaComoEstas");
+		Assert.assertTrue(validador.EsValida(contrasenia));
+	}
+
+}
