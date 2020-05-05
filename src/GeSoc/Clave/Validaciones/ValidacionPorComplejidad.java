c66fbd14b9cb92ce6ecd27f86543c41e4264065a
Marco Castillo <marco.andres.cricelli@gmail.com>
2020-05-05 01:28:41 -0300

    Se implementa criterio por complejidad, considerando numeros, mayusculas y caracteres especiales

--- src/GeSoc/Clave/Validaciones/ValidacionPorComplejidad.java
+++ src/GeSoc/Clave/Validaciones/ValidacionPorComplejidad.java
@@ -3,17 +3,48 @@ package GeSoc.Clave.Validaciones;
 import GeSoc.Clave.CriterioValidacion;
 
 public class ValidacionPorComplejidad implements CriterioValidacion {
+	
+	private String mensajeUsarNumeros = "La clave debe tener al menos un número.";
+	
+	private String mensajeUsarMayuscula = "La clave debe tener al menos una mayúscula.";
 
+	private String mensajeUsarCaracterEspecial = "La clave debe tener al menos un caracter especial (!@#$%^&*()+-=[]{};':\"|,.<>/?).";
+
+	
 	@Override
-	public Boolean EsValida(String clave) {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean EsValida(String clave)
+	{
+		return TieneNumero(clave) && TieneMayuscula(clave) && TieneCaracterEspecial(clave);
 	}
 
 	@Override
-	public String ObtenerMotivoInvalidez(String clave) {
-		// TODO Auto-generated method stub
-		return null;
+	public String ObtenerMotivoInvalidez(String clave)
+	{
+		// TODO: la interfaz deberia soportar un List<String> en vez de solo String para este metodo.
+		String motivo = "";
+		
+		if (!TieneNumero(clave)) motivo += mensajeUsarNumeros;
+		
+		if (!TieneMayuscula(clave)) motivo += mensajeUsarMayuscula;
+		
+		if (!TieneCaracterEspecial(clave)) motivo += mensajeUsarCaracterEspecial;
+		
+		return motivo;
+	}
+	
+	private Boolean TieneNumero(String clave)
+	{
+		return clave.matches(".*[0-9].*");
+	}
+	
+	private Boolean TieneMayuscula(String clave)
+	{
+		return clave.matches(".*[A-Z].*");
+	}
+	
+	private Boolean TieneCaracterEspecial(String clave)
+	{
+		return clave.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*");
 	}
 
 }
