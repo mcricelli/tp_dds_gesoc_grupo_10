a7c776e14d2e9fd11279d7f15878a1d79885a61f
Jose_Martinez <martinez022jose@gmail.com>
2020-05-04 23:11:31 -0300

    Se modifica 'validacionPorLongitud'

--- src/GeSoc/Clave/Validaciones/ValidacionPorLongitud.java
+++ src/GeSoc/Clave/Validaciones/ValidacionPorLongitud.java
@@ -3,46 +3,32 @@ package GeSoc.Clave.Validaciones;
 import GeSoc.Clave.CriterioValidacion;
 
 public class ValidacionPorLongitud implements CriterioValidacion {
-	char[] elementos={'0','1','2','3','4','5','6','7','8','9' ,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t',
-			'u','v','w','x','y','z'};
-	String claveGenerada;
+	private int minimo = 8;
+	private int maximo = 64;
+	private String errorMinimo = "El minimo de caracteres permitidos es: ";
+	private String errorMaximo = " El maximo de caracteres permitidos es: ";
 
 	@Override
 	public Boolean EsValida(String clave) {
-		if(this.esCreadaPorUsuario(clave)){
-			return true;
-		}else {
-			claveGenerada = this.generarContrasenia(clave);
-			return false;
-			
-		}
+		return this.estaEnElRango(clave);
 	}
 	
-	public String generarContrasenia(String clave){
-		char [] conjunto = new char[6];
-		for(int i=0;i<7;i++){
-			int el = (int)(Math.random()*37);
-			conjunto[i] = elementos[el];
-		}
-		return clave = new String(conjunto);
+	public Boolean estaEnElRango(String clave) {
+		return this.eliminarEspacioEnBlanco(clave).length() >= minimo && this.eliminarEspacioEnBlanco(clave).length()<=maximo;
 	}
 	
-	public boolean esCreadaDeFormaAleatoria(String clave) {
-		return clave.length()>6;
-	}
-	
-	public boolean esCreadaPorUsuario(String clave){
-		return clave.length()>8;
-		
-	}
 
 	@Override
 	public String ObtenerMotivoInvalidez(String clave) {
-		if(this.esCreadaPorUsuario(clave)) {
-			return "No respeta la longitud minima por usuario";
+		if(clave.length()<minimo) {
+			return errorMinimo+minimo;
 		}else {
-			return "Error al generar clave aleatoria";
+			return errorMaximo+maximo;
 		}
+	}
+	
+	public String eliminarEspacioEnBlanco(String clave) {
+		return clave.replaceAll("\\s","").trim();
 	}
 
 }
