90d0707852d4e49aeef850948d0aa913a82d2c8c
Jose_Martinez <martinez022jose@gmail.com>
2020-05-05 09:14:24 -0300

    Se agrega la totalidad de 'ValidacionPorLongitud'

--- src/GeSoc/Clave/Validaciones/ValidacionPorLongitud.java
+++ src/GeSoc/Clave/Validaciones/ValidacionPorLongitud.java
@@ -3,17 +3,31 @@ package GeSoc.Clave.Validaciones;
 import GeSoc.Clave.CriterioValidacion;
 
 public class ValidacionPorLongitud implements CriterioValidacion {
+	private int minimo = 8;
+	private int maximo = 64;
+	private String errorMinimo = "El minimo de caracteres permitidos es: ";
+	private String errorMaximo = " El maximo de caracteres permitidos es: ";
 
 	@Override
 	public Boolean EsValida(String clave) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.estaEnElRango(clave);
 	}
-
+	
+	public Boolean estaEnElRango(String clave) {
+		return this.eliminarEspacioEnBlanco(clave).length() >= minimo && this.eliminarEspacioEnBlanco(clave).length()<=maximo;
+	}
+	
 	@Override
 	public String ObtenerMotivoInvalidez(String clave) {
-		// TODO Auto-generated method stub
-		return null;
+		if(clave.length()<minimo) {
+			return errorMinimo+minimo;
+		}else {
+			return errorMaximo+maximo;
+		}
+	}
+	
+	public String eliminarEspacioEnBlanco(String clave) {
+		return clave.replaceAll("\\s","").trim();
 	}
 
 }
