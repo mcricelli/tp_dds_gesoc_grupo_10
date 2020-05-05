426d7803c28bc709fd418809e63027ed75b3fa52
Marco Castillo <marco.andres.cricelli@gmail.com>
2020-05-04 22:52:29 -0300

    Se implementa validación que compara contra las peores claves

--- src/GeSoc/Clave/Validaciones/ValidacionCompararContraPeores.java
+++ src/GeSoc/Clave/Validaciones/ValidacionCompararContraPeores.java
@@ -1,19 +1,41 @@
 package GeSoc.Clave.Validaciones;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import GeSoc.Clave.CriterioValidacion;
 
 public class ValidacionCompararContraPeores implements CriterioValidacion {
+	
+	private String archivoPeoresClaves = "./resources/claves_mas_comunes.txt";
 
+	private String motivoInvalidez = "La clave es demasiado común.";
+	
 	@Override
 	public Boolean EsValida(String clave) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		try {
+			FileInputStream fis = new FileInputStream(archivoPeoresClaves);			
+			Scanner sc = new Scanner(fis);
+			
+			while (sc.hasNextLine())  
+			{  
+				if (sc.nextLine().equals(clave))
+					return false;
+			}  
+			sc.close();
+			
+		} catch (FileNotFoundException e) {
+			return true;
+		}
+		
+		return true;
 	}
 
 	@Override
 	public String ObtenerMotivoInvalidez(String clave) {
-		// TODO Auto-generated method stub
-		return null;
+		return motivoInvalidez;
 	}
 
 }
